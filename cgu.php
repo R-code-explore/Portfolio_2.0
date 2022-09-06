9e1e2db40ffdfa054b5a60ce55960ba17179f25c
r_code_explore <rciupercher@gmail.com>
2022-09-06 10:49:55 +0200

    front update

---
+++ cgu.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CGU- Raimond-code</title>
+</head>
+<body>
+    
+<button class="index_btn"><a href="./index.php">Retour à l'accueil</a></button>
+
+<h1>Contact, formulaire et cookies</h1>
+
+<p>- Partie cookies : Lorsque vous naviguez sur le site, un cookie temporaire sera créé afin de persister votre état de connexion. Ce cookie sera automatiquement détruit lorsque vous vous déconnecterez du site.
+    <br>
+- Partie formulaire : Lorsqu'un message est envoyé par le biais du formulaire disponible sur le site, les informations personnelles que vous renseignez sont temporairement stockées. Ces dernières serviront à vous recontacter (comme le précise le message de remerciement après chaque envois). Une fois cela fait, les données sont effacées.</p>
+
+<h1>Mesures d'audience et statistiques</h1>
+
+<p>- Afin d'analyser le trafic du site, comme nombre de visites, pages les plus consultées, un service tiers proposé par l’hébergeur Ionos de chez 1&1 est utilisé. Les données sont anonymisées et ne permettent pas d'identifier le comportement individuel d'un utilisateur mais plutôt une tendance générale (nombre de visites sur le site et le nombre de visites par page seulement).</p>
+
+<h1>Vos droits sur vos données</h1>
+
+<p>- Vous pouvez demander la suppression des données personnelles vous concernant, bien que cette opération est déjà prise en compte et effectuée une fois recontacter, par le biais d'un simple message via la page d'accueil du site. Cela ne prend pas en compte les données stockées à des fins administratives, légales ou pour des raisons de sécurité.</p>
+
+<h1>Transmission de vos données personnelles</h1>
+
+<p>- Vos données personnelles sont nullement partagées.</p>
+
+<h1>Informations complémentaires</h1>
+
+<p>- Afin d'assurer la sécurité de vos données personnelles, temporairement stockées dans la base de données, des mesures de sécurité (pare-feu, base de données inaccessible et non distribuable par des tiers) proportionnelles à la sensibilité des données stockées ont été mises en place.</p>
+
+<button class="index_btn"><a href="./index.php">Retour à l'accueil</a></button>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,16 +1,9 @@
 <?php
 
 $page_title = "Raimond-code.com / Accueil";
-$css_link = './css/style.css';
-$anim_css_link = './css/anime_style.css';
+$css_link = "./css/style.css";
 
-include './includes/header.html';
-
-include './includes/banner_index.html';
-
-include './includes/services_index.html';
-
-include './includes/prev_projects.php';
+include './includes/header.php';
 ?>
 
 <?php
